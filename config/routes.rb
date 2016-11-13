d8ecfb4a17604361e9c156ed67e2ae70b314d335
Lillian Chou <lillianchou94@gmail.com>
2016-11-13 11:17:51 +0000

    updated google and twitter apis

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   root 'protests#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  
+  get 'google27b5178361bfde3d.html' => 'protests#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
